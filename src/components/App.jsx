93fc81da2828df89d274e997648c851fdff3bb05
InnaUsenko <innausenko8@gmail.com>
2023-10-20 13:48:41 +0300

    add Navagation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Link, Routes, Route } from 'react-router-dom';
 import { Home } from './Home/Home';
 import { Movies } from './Movies/Movies';
 import { MovieDetails } from './MovieDetails/MovieDetails';
 import { Cast } from './Cast/Cast';
 import { Reviews } from './Reviews/Reviews';
 
+const APP_PATH = '/goit-react-hw-05-movies';
 export const App = () => {
   return (
     <div>
+      <nav>
+        <Link to={`${APP_PATH}/`} style={{ marginRight: 24 }}>
+          Home
+        </Link>
+        <Link to={`${APP_PATH}/movies`} style={{ marginRight: 24 }}>
+          Movies
+        </Link>
+        <Link to={`${APP_PATH}/movies/1`} style={{ marginRight: 24 }}>
+          MovieDetails
+        </Link>
+      </nav>
       <Routes>
-        <Route path="/goit-react-hw-05-movies/" element={<Home />} />
-        <Route path="/goit-react-hw-05-movies/movies" element={<Movies />} />
-        <Route
-          path="/goit-react-hw-05-movies/movies/:movieId"
-          element={<MovieDetails />}
-        >
+        <Route path={`${APP_PATH}/`} element={<Home />} />
+        <Route path={`${APP_PATH}/movies`} element={<Movies />} />
+        <Route path={`${APP_PATH}/movies/:movieId`} element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -3,6 +3,21 @@ import { Link, Outlet } from 'react-router-dom';
 export const MovieDetails = props => {
   return (
     <main>
+      <ul
+        style={{
+          display: 'flex',
+          gap: 24,
+          listStyle: 'none',
+        }}
+      >
+        <li>
+          <Link to="cast">Read about cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Read about reviews</Link>
+        </li>
+      </ul>
+
       <h2>MovieDetails place is here</h2>
       <Outlet />
     </main>
